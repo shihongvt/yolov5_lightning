a059bf81e9a31a2c20aa1f1b77e261f0cf3d2117
shihongvt <shihong@vt.edu>
2023-08-18 09:59:54 -0400

    08/18/2023

--- yolov5_lightning.py
+++ yolov5_lightning.py
@@ -10,7 +10,8 @@ from torch.optim import Adam
 import torch.nn as nn
 from pytorch_lightning.callbacks import ModelCheckpoint
 
-# Datasets
+
+# Dataset Directory Structure:
 
 """ 
 
@@ -30,6 +31,9 @@ image2.jpg,45.1
 
 """
 
+
+# CustomDataset Class: fetch individual image-label pairs (given an index)
+
 class CustomDataset(Dataset):
     def __init__(self, data, transform=None):
         self.data = data
@@ -39,14 +43,12 @@ class CustomDataset(Dataset):
         return len(self.data)
 
     def __getitem__(self, idx):
-        # Check if self.data is a Subset
         if isinstance(self.data, torch.utils.data.Subset):
-            idx = self.data.indices[idx]  # Convert Subset index to original DataFrame index
+            idx = self.data.indices[idx]  
             actual_data = self.data.dataset
         else:
             actual_data = self.data
 
-        # Now access the data
         img_path = actual_data.iloc[idx]["image_path"]
         label_value = actual_data.iloc[idx]["label"]
 
@@ -55,6 +57,8 @@ class CustomDataset(Dataset):
             image = self.transform(image)
         return image, label_value
 
+
+# CustomDataModule Class: split the dataset into training and validation subsets and provides DataLoaders for them
 
 class CustomDataModule(pl.LightningDataModule):
     def __init__(self, image_folder, label_file, transform, batch_size=4, val_split=0.2):
@@ -78,21 +82,19 @@ class CustomDataModule(pl.LightningDataModule):
         return DataLoader(CustomDataset(self.val_data, transform=self.transform), batch_size=self.batch_size, shuffle=False)
 
 
-
-
-# Model architecture
+# Model architecture: custom regression model built on top of a pretrained YOLOv5 backbone
 
 class YOLOv5Regression(pl.LightningModule):
     def __init__(self):
         super(YOLOv5Regression, self).__init__()
         self.yolov5_backbone = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
         
-        # Use only the layers up to index 23 (excluding the detection head)
         self.features = nn.Sequential(*list(self.yolov5_backbone.model.children())[:-1][:24])
         
+        # a custom head for regression
         self.regression_head = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(512 * 60 * 40, 512),  # Adjusted the input size
+            nn.Linear(512 * 60 * 40, 512), 
             nn.ReLU(),
             nn.Dropout(0.5),
             nn.Linear(512, 1)
@@ -133,8 +135,6 @@ class YOLOv5Regression(pl.LightningModule):
         return optimizer
 
 
-#----- Now you are the user -----#
-
 transform = transforms.Compose([
     transforms.Resize((640, 640)),  
     transforms.ToTensor(),
@@ -154,7 +154,7 @@ checkpoint_callback = ModelCheckpoint(
     monitor='avg_val_loss',   # Save the model with the best validation loss
     dirpath='/home/shihong/data/mock_lightning/saved_models/',  # Directory where you want to save the model
     filename='model-{epoch:02d}-{avg_val_loss:.2f}',  # Naming scheme
-    save_top_k=3,  # Save only the top 3 models (based on the monitor metric, i.e., 'avg_val_loss' here)
+    save_top_k=3,  # Save only the top 3 models
     mode='min',  # 'min' for metrics where lower is better (like loss), 'max' for metrics where higher is better (like accuracy)
 )
 
